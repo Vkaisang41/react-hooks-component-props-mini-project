1530ab7095850ebe41616f5be32dff7ae8d7f118
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-07-10 14:44:23 +0300

    Finish Components and Props Lab – all tests passing

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const getEmojiDisplay = () => {
+    if (minutes < 30) {
+      return "☕️".repeat(Math.ceil(minutes / 5));
+    } else {
+      return "🍱".repeat(Math.ceil(minutes / 10));
+    }
+  };
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} {getEmojiDisplay()} {minutes} min read</small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
